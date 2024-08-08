3457f34cbaff3a67a1fef5e6df28556ba3724ede
Shanu <shanu@Shanus-MacBook-Pro.local>
2024-08-08 11:02:03 +0530

    Changes

--- app/myreservations/page.tsx
+++ app/myreservations/page.tsx
@@ -1,25 +1,17 @@
 import Image from "next/image";
-
 import apiService from "../services/apiService";
-import Link  from "next/link";
+import Link from "next/link";
 
-
-const MyReservationPage = async () => {
+const MyReservationsPage = async () => {
     const reservations = await apiService.get('/api/auth/myreservations/')
 
-    return(
+    return (
         <main className="max-w-[1500px] mx-auto px-6 pb-6">
-            
-                <h1 className="my-6 text-2xl">
-                    My Reservations
-                </h1>
+            <h1 className="my-6 text-2xl">My reservations</h1>
 
             <div className="space-y-4">
-
                 {reservations.map((reservation: any) => {
-
-                    return (                  
-                   
+                    return (              
                         <div className="p-5 grid grid-cols-1 md:grid-cols-4 gap-4 shadow-md border border-gray-300 rounded-xl">
                             <div className="col-span-1">
                                 <div className="relative overflow-hidden aspect-square rounded-xl">
@@ -27,33 +19,33 @@ const MyReservationPage = async () => {
                                         fill
                                         src={reservation.property.image_url}
                                         className="hover:scale-110 object-cover transition h-full w-full"
-                                        alt="Beach House" />
+                                        alt="Beach house"
+                                    />
                                 </div>
                             </div>
 
                             <div className="col-span-1 md:col-span-3">
                                 <h2 className="mb-4 text-xl">{reservation.property.title}</h2>
 
-                                <p mb-2><strong>Check in Date:</strong> {reservation.start_date}</p>
-                                <p mb-2><strong>Check out Date:</strong> {reservation.end_date}</p>
-                                <p mb-2><strong>Number of Nights:</strong> {reservation.number_of_nights}</p>
-                                <p mb-2><strong>Total Price:</strong> {reservation.total_price}</p>
+                                <p className="mb-2"><strong>Check in date:</strong> {reservation.start_date}</p>
+                                <p className="mb-2"><strong>Check out date:</strong> {reservation.end_date}</p>
+
+                                <p className="mb-2"><strong>Number of nights:</strong> {reservation.number_of_nights}</p>
+                                <p className="mb-2"><strong>Total price:</strong> ${reservation.total_price}</p>
 
                                 <Link 
                                     href={`/properties/${reservation.property.id}`}
                                     className="mt-6 inline-block cursor-pointer py-4 px-6 bg-airbnb text-white rounded-xl"
                                 >
-                                    Go to Property
+                                    Go to property
                                 </Link>
-
                             </div>
                         </div>
                     )
                 })}
-                
             </div>
-        
         </main>
     )
 }
-export default MyReservationPage;
+
+export default MyReservationsPage;
